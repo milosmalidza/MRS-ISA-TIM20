1d80f95414c3b87723c62cdd0db1e2554cbfeb1c
Milos Malidza <milosmalidza@gmail.com>
2019-03-22 01:26:20 +0100

    Odradjena forma za pretrazivanje vozila

---
+++ WebApplication/src/main/resources/static/main-script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function() {
+	var mainDiv = document.getElementById("main-div");
+	if (mainDiv != "undefined") {
+		mainDiv.style.opacity = "1";
+		mainDiv.style.webkitTransform = "scale(1)";
+		mainDiv.style.transform = "scale(1)";
+	}
+	
+}, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
