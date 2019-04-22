36ef9626bbef5b0a902e55d3452fd454af9c2c30
bojanVlasonjic <bojanvlasonjic@yahoo.com>
2019-04-22 18:14:23 +0200

    Ispravljena mala greskica kod dodavanja vise administratora

--- AHCReservs/src/main/resources/static/js/system-admin.js
+++ AHCReservs/src/main/resources/static/js/system-admin.js
@@ -102,11 +102,14 @@ async function axiosAdmins(serverMethodPath) {
 
 async function viewAdmins() {
 	
-	//clearDisplayedAdmins
+	
+	// clear displayed admins
+	$("#available-admins").dropdown('clear');
 	var multiSelect = document.getElementById("available-admins");
 	multiSelect.selectedIndex = -1;
 	$('#available-admins option:selected').remove();
 	$('#available-admins').empty().append('<option value="">Select admin</option>');
+	
 	
 	/* Get admins */
 	let companyType = $("#company-type-form2").val();
@@ -126,6 +129,12 @@ async function viewAdmins() {
 }
 
 async function addAdmins() {
+	
+	
+	if(validateInputFields("#company-admins-table") === false) {
+		toast("Empty field detected");
+		return;
+	}
 	
 	let companyName = $("#admins-company-name").val();
 	
